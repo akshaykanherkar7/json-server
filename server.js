c5bba086387c46f57a2ae756a8eb6d76e31bcb5a
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-07-20 22:49:26 +0530

    Heroku Json Server

---
+++ server.js
@@ -0,0 +1,10 @@
+const PORT = process.env.PORT || 3000;
+const path = require("path");
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(_dirname, "db.json"));
+const middlewares = jsonServer.defaults();
+server.use(middlewares);
+server.use(jsonServer.bodyParser);
+server.use("/api", router);
+server.listen(PORT, () => console.log("Running on port 3000"));
