381bd2b9e1166d10c1b270455d6518744a0d2886
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-07-20 22:59:26 +0530

    Heroku Json Server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 3000;
 const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(_dirname, "db.json"));
+const router = jsonServer.router(path.join(__dirname, "db.json"));
 const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(jsonServer.bodyParser);
